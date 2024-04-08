14697b21acbcf67758e81ae4e69d5b4ea006de6c
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2024-04-08 19:18:26 +0700

    sử dụng canvas để vẽ

--- main.js
+++ main.js
@@ -1,86 +1,96 @@
 // 21130035_Nguyễn_Ngọc_Khánh_Duy_0839151003
 
-// const food = document.querySelector(".board__food")
-// const snake = document.querySelector(".board__snake")
+// canvas
+var recSize = 25
+var rows = 20
+var cols = 20
+var board;
+var context;
+
 // Toạ độ food
 let foodX = 0;
 let foodY = 0;
 // Toạ độ rắn
-let snakeX = 10;
-let snakeY = 10;
+// let snakeX = 0;
+// let snakeY = 0;
+let snakeX = recSize;
+let snakeY = recSize;
 let moveX = 0;
 let moveY = 0;
 let snakeBody = []
-// food.style.display = "block"
-// row / column
-// snake.style.gridArea = snakeY+"/"+snakeX
 
 // Hàm tạo food ngẫu nhiên
 function randomFood() {
-    foodX = Math.floor(Math.random() * 20 + 1)
-    foodY = Math.floor(Math.random() * 20 + 1)
-    // food.style.gridArea = foodY+"/"+foodX;
+    // foodX = Math.floor(Math.random() * 20 + 1)
+    // foodY = Math.floor(Math.random() * 20 + 1)
+
+    foodX = Math.floor(Math.random() * cols) * recSize
+    foodY = Math.floor(Math.random() * rows) * recSize
 }
 // randomFood()
 
 // Hàm di chuyển theo hướng mũi tên
 function changeDirection(e) {
     // trái, lên, phải, xuống === 37, 38, 39, 40
-    switch (e.keyCode){
-        case 37:
-            moveX = -1
-            moveY = 0
-            break
 
-        case 38:
-            moveX = 0
-            moveY = -1
-            break
-
-        case 39:
-            moveX = 1
-            moveY = 0
-            break
-
-        case 40:
-            moveX = 0
-            moveY = 1
-            break
-
-        default:
-            moveX = 0
-            moveY = 0
-            break
+    if (e.keyCode == 37 && moveX != 1) {
+        moveX = -1
+        moveY = 0
+    }else if (e.keyCode == 38 && moveY != 1){
+        moveX = 0
+        moveY = -1
+    }else if(e.keyCode == 39 && moveX != -1){
+        moveX = 1
+        moveY = 0
+    }else if (e.keyCode == 40 && moveY != -1){
+        moveX = 0
+        moveY = 1
     }
 }
 
+window.onload = function () {
+    board = document.getElementById("board")
+    board.height = rows * recSize
+    board.width = cols * recSize
+    context = board.getContext("2d")
+}
+
 function moveSnake() {
-    let htmlSnake = `<div class="board__food" style="grid-area: ${foodY} / ${foodX}"></div>`
+
+    context.fillStyle="black"
+    context.fillRect(0, 0, board.width, board.height)
+
+    context.fillStyle="red"
+    context.fillRect(foodX, foodY, recSize, recSize)
+
+    // let htmlSnake = `<div class="board__food" style="grid-area: ${foodY} / ${foodX}"></div>`
 
     if (snakeX === foodX && snakeY === foodY) {
-        console.log(snakeBody)
-        console.log([foodX, foodY])
         randomFood()
         snakeBody.push([foodX, foodY])
-        console.log(snakeBody)
-       clearInterval(te)
     }
 
     for (let i = snakeBody.length - 1; i > 0 ; i--) {
         snakeBody[i] = snakeBody[i-1]
     }
 
-    snakeX += moveX
-    snakeY += moveY
+    // snakeX += moveX
+    // snakeY += moveY
+    snakeX += (moveX * recSize)
+    snakeY += (moveY * recSize)
 
     snakeBody[0] = [snakeX, snakeY]
 
-    for (let i = 0; i < snakeBody.length; i++) {
-        htmlSnake += `<div class="board__snake ${i}" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`
-    }
-    document.querySelector(".board").innerHTML = htmlSnake
+    // for (let i = 0; i < snakeBody.length; i++) {
+    //     htmlSnake += `<div class="board__snake ${i}" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`
+    // }
+    // document.querySelector(".board").innerHTML = htmlSnake
 
-    // snake.style.gridArea = snakeY+"/"+snakeX
+    context.fillStyle="green"
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], recSize, recSize)
+    }
+
 }
 
 randomFood()
